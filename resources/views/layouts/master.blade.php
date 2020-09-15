3feca334dce78cd4703abedca6a8ad2b9e72003c
juniarsofyan <choerulsofyanmf@gmail.com>
2020-09-16 05:59:04 +0700

    Add javascript for confirming deletion

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -118,6 +118,8 @@
     {{-- <script src="{{ asset('dist/js/pages/dashboard.js') }}"></script> --}}
     <!-- AdminLTE for demo purposes -->
     <script src="{{ asset('dist/js/demo.js') }}"></script>
+    
+    <script src="{{ asset('assets/js/global.js') }}"></script>
 
     @yield('scripts')
 </body>
